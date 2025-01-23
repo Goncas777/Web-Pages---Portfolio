12dcf8317da9c82aa4eefe0a3a704f17f09652d7
Goncas777 <123480612+Goncas777@users.noreply.github.com>
2025-01-23 15:06:26 +0000

    final version, let's pray to get at least a 17

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+async function loadFooter() {
+  const response = await fetch("components/footer.html");
+  const navbarHtml = await response.text();
+  document.getElementById("footer-container").innerHTML = navbarHtml;
+}
+
 async function loadNavbar() {
     const response = await fetch("components/navbar.html");
     const navbarHtml = await response.text();
     document.getElementById("navbar").innerHTML = navbarHtml;
   }
   
-  async function loadFooter() {
-    const response = await fetch("components/footer.html");
-    const navbarHtml = await response.text();
-    document.getElementById("footer").innerHTML = navbarHtml;
-  }
+
   
   loadFooter();
   loadNavbar();
